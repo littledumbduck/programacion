3c22f6bf906f5862a74790decc061a093f2c8adf
littledumbduck <ale0014@alu.medac.es>
2025-10-01 13:42:30 +0200

    01/10
    Todos los ejercicios del boletin_4 terminados.

--- primerproyecto/src/boletin_4/ejercicio20.java
+++ primerproyecto/src/boletin_4/ejercicio20.java
@@ -5,15 +5,29 @@ import java.util.Scanner;
 public class ejercicio20 {
 
 	public static void main(String[] args) {
-		
-		int num;
-		int intentos = 10;
+
+		int num, total = 0;
+		int media = 0;
+		int numerodenotas = 0, positivo = 0;
 		Scanner teclado = new Scanner(System.in);
-		
-		while (intentos>0) {
-			
+
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Teclee la nota:");
+			num = teclado.nextInt();
+
+			total = total + num;
+			numerodenotas++;
+			media = total / numerodenotas;
+
+			if (num > media) {
+				positivo++;
+			}
+
 		}
-		
+
+		teclado.close();
+		System.out.println("La media de notas es " + media + " y las notas superiores a la media es de " + positivo + " notas.");
+
 	}
 
 }
